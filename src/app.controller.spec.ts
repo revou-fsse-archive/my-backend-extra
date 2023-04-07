87d063f82e683505c1bb998161fb98451feaec2c
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-07 21:16:48 +0700

    feat: change get / to welcome

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return the welcome message', () => {
+      expect(appController.getHello()).toBe({
+        message: "Welcome to Median API"
+      });
     });
   });
 });
