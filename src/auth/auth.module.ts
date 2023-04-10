b35e9c1dafe3a326e7560909eaad96f4c4a4011c
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-11 00:23:37 +0700

    feat: setup auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { PrismaModule } from "src/prisma/prisma.module";
+
+export const jwtSecret = process.env.JWT_SECRET;
 
 @Module({
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: "5m" }, // e.g. 30s, 7d, 24h
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
