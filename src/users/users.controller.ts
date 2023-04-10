364fda314fbae7d992c234a596f63c675eb35cce
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-11 01:36:24 +0700

    feat: implement JWT auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from "@nestjs/common";
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 
@@ -13,6 +14,8 @@ import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { UserEntity } from "./entities/user.entity";
+
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
 @Controller("users")
 @ApiTags("users")
@@ -26,6 +29,7 @@ export class UsersController {
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   @ApiOkResponse({ type: UserEntity, isArray: true })
   async findAll() {
     const users = await this.usersService.findAll();
@@ -33,18 +37,21 @@ export class UsersController {
   }
 
   @Get(":id")
+  @UseGuards(JwtAuthGuard)
   @ApiOkResponse({ type: UserEntity })
   async findOne(@Param("id") id: string) {
     return new UserEntity(await this.usersService.findOne(id));
   }
 
   @Patch(":id")
+  @UseGuards(JwtAuthGuard)
   @ApiCreatedResponse({ type: UserEntity })
   async update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     return new UserEntity(await this.usersService.update(id, updateUserDto));
   }
 
   @Delete(":id")
+  @UseGuards(JwtAuthGuard)
   @ApiOkResponse({ type: UserEntity })
   async remove(@Param("id") id: string) {
     return new UserEntity(await this.usersService.remove(id));
