4ac66fd5e7e663b0b20d82ab796738ef6397b7ea
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-10 21:58:48 +0700

    feat: generate users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ArticlesModule } from "./articles/articles.module";
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule],
+  imports: [PrismaModule, ArticlesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
